a6b64200c03ef037792299cfff50b04abec2b4eb
kpcrtm <kpcrtm@gmail.com>
2015-04-25 19:32:49 -0700

    Initial check in of the programming assignment 2
    Initial check in of the programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,97 @@
-## Put comments here that give an overall description of what your
-## functions do
+#
+# This file contains two functions to cache the matrix inverse into another environment
+#       
+#	makeCacheMatrix <- function(x = matrix())
+#		creates a special matrix object for caching a matrix in another environment
+#	cacheSolve <- function(x, ...)
+#		Computes the matrix inverse or returns the version cached in another environment
+#		
+# I have also included a test function to test the caching of the matrix inverse
+#	testCache <- function()
+#
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+# makeCacheMatrix:
+#
+# Create a special matrix object or caching a matrix in another enviroment  The <<- operation is 
+# used to assign the object to another environment.
+#
+# Paramater: 
+#	x a square invertible matrix
+# Returns:
+#	list of functions to 
+#		- set the matrix
+#		- get the matrix
+#		- set the matric inverse
+#		- get the matric inverse
+#
+makeCacheMatrix <- function(x = matrix())
+{
+    inv <- NULL
+
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+
+    list(set=set, get=get,setinv=setinv, getinv=getinv)
+}
+
+#
+# cacheSolve:
+#
+# Computes the matrix inverse or returns the version cached in 
+# another environment
+#
+# Parameter:
+#	x the cachable matrix object created in makeCacheMatrix
+# Returns:
+#	The matrix inverse
+#
+cacheSolve <- function(x, ...)
+{
+    inv <- x$getinv()
+    
+    if (!is.null(inv)) {
+        return(inv)
+    }
+
+    data <- x$get()
+    inv <- solve(data, ...)
+    
+    x$setinv(inv)
+
+    inv
 }
 
 
-## Write a short comment describing this function
+#
+# testCache:
+#
+# This creates a 1000 x 1000 matrix and calculates the time to both
+# calculate the matrix inverse and to fetch it from the cache
+#
+testCache <- function()
+{
+	# Create a 1000 x 1000 invertible matrix
+	m <- matrix(rnorm(1000 * 1000), 1000, 1000)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	# Create the cacheable matrix object
+	mobj <- makeCacheMatrix(m)
+
+	# Calculate the matrix inverse
+	ptm <- proc.time()
+	cacheSolve(mobj)
+	t1 <- proc.time() - ptm
+	cat("Time to calculate matrix inverse: ", t1[1], "\n")
+
+	# Fetch the matrix inverse from another environment
+	ptm <- proc.time()
+	cacheSolve(mobj)
+	t2 <- proc.time() - ptm
+	cat("Time to fetch matrix inverse from cache: ", t2[1], "\n")
 }
